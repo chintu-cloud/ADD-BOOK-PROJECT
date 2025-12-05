e760641a79815d0962e5888001c85d2f48921f8a
CHANDAN MOHANTY <github.chintu02@gmail.com>
2025-12-05 17:31:43 +0530

    Create config.js

---
+++ client/src/pages/config.js
@@ -0,0 +1,7 @@
+const API_BASE_URL = "http://98.92.112.243:84"
+//const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
+// export default API_BASE_URL;
+// const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
+/// export default API_BASE_URL;
+//const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
+export default API_BASE_URL;
